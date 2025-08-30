14c78847c79468024f115a242151d28708f8e071
Ryan Weber <ryanweber88@gmail.com>
2025-08-30 02:29:56 -0400

    Some skeleton, base aliases

---
+++ profiles/aliases.sh
@@ -0,0 +1,12 @@
+# Cross-shell aliases & functions for portability.
+
+# Git QOL
+alias gs='git status'
+alias st='git status'
+alias gl='git log --oneline --graph --decorate -n 30'
+alias gp='git push'
+alias gpf='git push --force-with-lease'
+alias gb='git branch'
+alias gco='git checkout'
+alias gd='git diff'
+alias diff='git diff'
